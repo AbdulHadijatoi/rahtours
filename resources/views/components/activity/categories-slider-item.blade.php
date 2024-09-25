f17ee53ab0370bc0fcf483b62dd566cc32f6f441
Abdul Hadi <Abdulhadijatoi@gmail.com>
2024-09-25 07:28:17 +0500

    updated categories

--- resources/views/components/activity/categories-slider-item.blade.php
+++ resources/views/components/activity/categories-slider-item.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ url('dubai-activities/'. $category->slug) }}" class="flex flex-col items-center w-[10rem]">
-    <div class="w-[5rem] h-[5rem] bg-cover bg-center rounded-full" style="background-image: url('{{ url($category->image) }}')"></div>
-    <p class="mt-2 font-semibold text-xs text-center">{{ $category->name }}</p>
+<a href="{{ url('dubai-activities/'. $category->slug) }}" class="flex flex-col items-center min-w-[10rem]">
+    <div class="w-[5rem] h-[5rem] bg-cover bg-center rounded-full {{ isActive($category->slug) ? 'border-4 border-yellow-600' : '' }}" style="background-image: url('{{ url($category->image) }}')"></div>
+    <p class="mt-2 font-semibold text-xs text-center {{ isActive($category->slug) ? 'text-secondary' : '' }}">{{ $category->name }}</p>
 </a>
--- resources/views/components/activity/categories-slider.blade.php
+++ resources/views/components/activity/categories-slider.blade.php
@@ -1,20 +1,20 @@
 <div class="relative flex items-center justify-between mx-1 my-5 md:m-10">
     <!-- Left arrow -->
-    <button id="scrollLeft" class="absolute left-0 z-10 bg-white rounded-full p-2 border">
+    <button id="scrollLeft" class="absolute left-0 z-10 bg-white rounded-full p-2 bg-yellow-50">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
         </svg>
     </button>
 
     <!-- Slider container -->
-    <div id="slider" class="flex space-x-10 md:space-x-16 overflow-x-scroll scroll-smooth p-4 hide-scrollbar mx-10">
+    <div id="slider" class="flex flex-row overflow-x-scroll scroll-smooth hide-scrollbar mx-14">
         @foreach (getCategories() as $category)
             @include('components.activity.categories-slider-item')
         @endforeach
     </div>
 
     <!-- Right arrow -->
-    <button id="scrollRight" class="absolute right-0 z-10 bg-white rounded-full p-2 border">
+    <button id="scrollRight" class="absolute right-0 z-10 bg-white rounded-full p-2 bg-yellow-50">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
         </svg>
