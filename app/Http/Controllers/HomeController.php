34c4c69afc9a0c3c442bd41c8362d576649f4823
Abdul Hadi <Abdulhadijatoi@gmail.com>
2024-09-26 22:17:18 +0500

    fixes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller {
     
     public function index($slug = null) {
-        $mostPopularActivities = Activity::where('most_popular_activity', 1)->get();
+        $mostPopularActivities = Activity::where('most_popular_activity', 1)->orderBy('created_at','DESC')->get();
 
         return view('home-page', compact('mostPopularActivities'));
     }
