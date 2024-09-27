5c55f28ae1356b3548214a6ebe32f8ac85b598a3
Abdul Hadi <Abdulhadijatoi@gmail.com>
2024-09-27 09:50:00 +0500

    updated svg width in admin side

--- resources/views/admin/category/update.blade.php
+++ resources/views/admin/category/update.blade.php
@@ -8,31 +8,31 @@
                     <div class="card-body">
                         <h4 class="card-title">Update Category</h4>
                         <div class="container mx-auto mt-4">
-    @if(session('success'))
-        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
-            <strong class="font-bold">Success!</strong>
-            <span class="block sm:inline">{{ session('success') }}</span>
-            <span class="w-5 h-5 absolute top-0 bottom-0 right-0 px-4 py-3">
-                <svg class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M10 9l3.5-3.5L15 7l-3.5 3.5L15 14l-1.5 1.5L10 12l-3.5 3.5L5 14l3.5-3.5L5 7l1.5-1.5L10 9z"/></svg>
-            </span>
-        </div>
-    @endif
+                            @if(session('success'))
+                                <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+                                    <strong class="font-bold">Success!</strong>
+                                    <span class="block sm:inline">{{ session('success') }}</span>
+                                    <span class="w-5 h-5 absolute top-0 bottom-0 right-0 px-4 py-3">
+                                        <svg style="width: 25px !important;" class="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M10 9l3.5-3.5L15 7l-3.5 3.5L15 14l-1.5 1.5L10 12l-3.5 3.5L5 14l3.5-3.5L5 7l1.5-1.5L10 9z"/></svg>
+                                    </span>
+                                </div>
+                            @endif
 
-    @if($errors->any())
-        <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-            <strong class="font-bold">Whoops!</strong>
-            <span class="block sm:inline">Please fix the following errors:</span>
-            <ul class="list-disc pl-5">
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-            <span class="w-5 h-5 absolute top-0 bottom-0 right-0 px-4 py-3">
-                <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M10 9l3.5-3.5L15 7l-3.5 3.5L15 14l-1.5 1.5L10 12l-3.5 3.5L5 14l3.5-3.5L5 7l1.5-1.5L10 9z"/></svg>
-            </span>
-        </div>
-    @endif
-</div>
+                            @if($errors->any())
+                                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                                    <strong class="font-bold">Whoops!</strong>
+                                    <span class="block sm:inline">Please fix the following errors:</span>
+                                    <ul class="list-disc pl-5">
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                    <span class="w-5 h-5 absolute top-0 bottom-0 right-0 px-4 py-3">
+                                        <svg style="width: 25px !important;" class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M10 9l3.5-3.5L15 7l-3.5 3.5L15 14l-1.5 1.5L10 12l-3.5 3.5L5 14l3.5-3.5L5 7l1.5-1.5L10 9z"/></svg>
+                                    </span>
+                                </div>
+                            @endif
+                        </div>
 
                         <form class="forms-sample" action="{{ route('admin.categories.update', $category->id) }}" method="post" enctype="multipart/form-data">
                             @csrf
